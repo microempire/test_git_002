ad826d6a395ab164744ebd816ebf7a33a7ee2a88
Than Zaw Toe <microempire37@gmail.com>
2022-09-23 15:38:38 +0630

    testing multiple input and multiple output

---
+++ test02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+int a,b,c;
+
+printf("Hello world\n");
+printf("pelase entre some digit: ");
+scanf("%d %d %d",&a ,&b ,&c);
+printf("a is %d b is %d c is %d",a ,b ,c);
+return 0 ;
+
+}
